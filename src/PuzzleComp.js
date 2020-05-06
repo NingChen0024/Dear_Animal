d39e604e0a60486c1eb1004feee9ea5c8a518847
Ning <nche0024@gmail.com>
2020-05-06 20:14:48 +1000

    new

--- src/PuzzleComp.js
+++ src/PuzzleComp.js
@@ -12,19 +12,6 @@ import Button from 'react-bootstrap/Button';
 
 class PuzzleComp extends Component{
 
-    // constructor(props) {
-    //     super(props);
-    //     this.state = { open: false };
-    //     this.openModal = this.openModal.bind(this);
-    //     this.closeModal = this.closeModal.bind(this);
-    //   }
-
-    //   openModal() {
-    //     this.setState({ open: true });
-    //   }
-    //   closeModal() {
-    //     this.setState({ open: false });
-    //   }
     
     render(){
 
