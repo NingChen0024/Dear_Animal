3b2cdea40c73cfcdff7295ae902610762a86dba6
Ning <nche0024@gmail.com>
2020-05-09 10:44:03 +1000

    new

--- src/About.js
+++ src/About.js
@@ -1,102 +1,89 @@
 
 import React ,{ Component }from 'react';
 import axios from 'axios'
-
+import $ from 'jquery';
 import Carousel from 'react-bootstrap/Carousel'
 
 
 class About extends Component {
 
-    state = {
-      loading:true,
-      pid: null,
-    }
-
-
-    async componentDidMount(){
-      axios.get('/api/get/allkidtips')
-      //.then(res => console.log(res.data))
-      .then(res => this.setState({pid: res.data, loading: false}))
-      .catch(err => console.log(err))
-    }
-
 
     render() {
 
-      console.log(this.state)
-      console.log(this.state.pid)
+    //   $(function(){
+    //     $("#postCodeForm").on('submit', function(e){
+    //       e.preventDefault();
+    //       //  Clear markers here
+    //         var matches = 0;
+    //         for (var i = 0; i < markers.length; i++) {
+    //             markers[i].setMap(null);
+    //         }
+    //         markers = [];
+
+    //         var postCode = document.getElementById('postCode').value;
+
+    //         var infowindow = new google.maps.InfoWindow({
+    //             content: ""
+    //         });
+    //         if (!animalData[postCode] || animalData[postCode].length == 0)
+    //             alert("No match found");
+    //         for (var i = 0; i < animalData[postCode].length; i++) {
+    //             console.log(animalData[postCode][i]);
+    //             matches += 1;
+    //             console.log('match');
+    //             // var name = []
+    //             // name.push(animalData[i]['Vernacular name - original'])
+    //             name = animalData[postCode][i]['Vernacular name - original'];
+    //             address = animalData[postCode][i]['address'];
+    //             console.log(name, address);
+    //             marker = new google.maps.Marker({ position: { title: name + address, lat: parseFloat(animalData[postCode][i]['Latitude']), lng: parseFloat(animalData[postCode][i]['Longitude']) }, map: map });
+
+    //             content = '<h3>' + name + '</h3><p>' + address + '</p><a href="https://www.google.com/maps/dir//'+address+'" target="_blank">Directions</a>';
+
+    //             // marker.addListener('click', function () {
+    //             //     infowindow.setContent(marker.content);
+    //             //     infowindow.open(map, marker);
+    //             // });
+
+    //             google.maps.event.addListener(marker, 'click', (function (marker, content, infowindow) {
+    //                 return function () {
+    //                     infowindow.setContent(content);
+    //                     infowindow.open(map, marker);
+    //                 };
+    //             })(marker, content, infowindow));
+
+
+    //             markers.push(marker);
+
+    //             // Add infowindow HERE
+    //         }
+
+    //         var bounds = new google.maps.LatLngBounds();
+    //         for (var i = 0; i < markers.length; i++) {
+    //             bounds.extend(markers[i].position);
+    //         }
+
+    //         map.fitBounds(bounds);
+
+
+    //         if (matches == 0)
+    //             window.alert('no match found');
+    //         console.log('call finished');
+    //         })
+    //   })
+        
+    // function initMap() {
+    //     markers = [];
+    //     var vic = { lat: -37.4713, lng: 144.7852 };
+    //     map = new google.maps.Map(
+    //         document.getElementById('map'), { zoom: 8, center: vic });
+    // }
+    
       return (
       <div >
-       {this.state.loading || !this.state.pid ? (
-         <div>loading....</div>
-       ) : (
-          <div className="container">
-            <Carousel>
-              {
-                this.state.pid.map(tip => (
-                  <Carousel.Item>
-                  <img src={ require("./images/kidtipsimg.png")} class="kidtipsImg" alt="office"/>
-                  <Carousel.Caption>
-                    <h1 className='kidtipsfont'>{tip.content}</h1>
-                  
-                  </Carousel.Caption>
-                </Carousel.Item>
-                ))
-              }
-            </Carousel>
-          </div>
-       )} 
-        {/* <p>123</p>
-        <div>
-          {this.state.pid
-          ? <div> 
-              <div>{this.state.pid[0].title}</div>
-            </div>
-          : <p>err</p>
-          }
-
-    
-        </div> */}
-          
-
-        {/* <div className="container">
-          <Carousel>
-            {
-              this.state.pid.map(pers => (
-                <Carousel.Item>
-                <img src={ require("./images/newsletter.png")} class="kidtipsImg" alt="office"/>
-                <Carousel.Caption>
-                  <h1>{pers.name}</h1>
-                 
-                </Carousel.Caption>
-              </Carousel.Item>
-              ))
-            }
-          </Carousel>
-        </div> */}
-
-          {/* <FlippingCard>
-              <FlippingCardBack>
-              Content that will be displayed on the back of the card
-            </FlippingCardBack>
-            <FlippingCardFront>
-              Content that will be displayed on the front of the card
-            </FlippingCardFront>
-        </FlippingCard> */}
-
-        {/* <TaggedContentCard
-          href='https://github.com/nukeop'
-          thumbnail='https://i.imgur.com/H0f0R6p.jpg'
-          title='Burger'
-          description='the cornerstone of every nutritious breakfast'
-          tags={[
-            'food',
-            'sandwich',
-            'meat'
-          ]}
-        /> */}
-
-
+        <h3>My Google Maps Demo</h3>
+        <form id="postCodeForm"><input type="text" id="postCode"/><button onClick="searchPostCode()">Search</button></form>
+        <div id="map"></div>
       </div>
       )
       
--- src/Anidetail.js
+++ src/Anidetail.js
@@ -4,12 +4,130 @@ import {Link} from 'react-router-dom'
 
 
 class Anidetail extends Component{
+
+    state = {
+        sound: false
+    }
+
     render(){
 
         const{data} = this.props.location
+        try{
+            var foo = require('./sounds/' + data.aniname + '.mp3')
+            let audio = new Audio('./sounds/' + data.aniname + '.mp3')
+            const start = () => {
+                audio.play()
+            }
+            this.setState({sound:true})
+        }catch(e){
+            this.setState({sound:false})
+        }
 
         return(
-            <dev className='anidetailbackground'>
+            <div>
+
+            {this.state.sound ? (
+                
+                <dev className='anidetailbackground'>
+                <div className='detail-container'>
+                <img src={require('./images/animal_list/' + data.aniname + '_cover.jpeg')} class="img-fluid" />
+                    <h1 className="bottom-right text-white unifont">{data.aniname}</h1>
+                    <p className="fact-bottom-right">{data.facts}</p>
+                </div>
+                
+                <div className="container row1-style mt-5 mb-3 ">
+                    <div className="row">
+
+                        <div className="col-lg-4 col-md-4 col-12 ">
+                            <p className='row1-font'>Type:{data.class}</p>
+                        </div>
+                            
+                        <div className="col-lg-4 col-md-4 col-12">
+                            <p className='row1-font'>Total Count:{data.anicount}</p>
+                        </div>
+
+                        <div className="col-lg-4 col-md-4 col-12 ">
+                            <p className='row1-font'>Status:{data.anistatus}</p>
+                        </div>
+                    </div>
+                </div>
+
+
+                <div className='m-5'>
+                    <img src={require('./images/animal_list/' + data.aniname + '_cards.jpeg')} className="img-fluid card-image" />
+                </div>
+
+
+                <div className='container mb-5'>
+                    <div className='row'>
+                        <div className="col-lg-6 col-md-6 col-12 ">
+                        <img src={require('./images/animal_list/' + data.aniname + '_detail.jpeg')} class="img-fluid" />
+                        </div>
+
+                        <div className="col-lg-6 col-md-6 col-12 ">
+                            <p className='row2-font'>{data.anidescription}</p>
+                            <button onClick={start} className='btn btn-warning btn-lg mt-3 unifont'>Let's hear his voice!</button>
+                        </div>
+                    </div>
+                </div>
+
+                <div className='container mb-5 pb-5'>
+                    <div className='row'>
+                        <div className="col-lg-3 col-md-3 col-12 back-button">
+
+                            <Link to='/animalList'>     
+                                <a class="btn btn-warning btn-lg mt-3 unifont"  data-aos-delay="100">Let's go back</a>
+                            </Link>
+
+                        </div>
+                        <div className="col-lg-3 col-md-3 col-12 "></div>
+                        <div className="col-lg-3 col-md-3 col-12 "></div>
+                        <div className="col-lg-3 col-md-3 col-12 forword-button">
+
+                            <Link to={{
+                                        pathname: "/games",
+                                        data: data
+                                    }}>    
+                                <a class="btn btn-warning btn-lg mt-3 unifont"  data-aos-delay="100">Let's play a game</a>
+                            </Link>
+
+                        </div>
+                    </div>
+                </div>
+                
+                <footer class="site-footer">
+                <div class="container">
+                    <div class="row">
+
+                    <div class="col-lg-5 mx-lg-auto col-md-8 col-10">
+                        <h1 class="text-white"  data-aos-delay="100">We are making the <strong>best </strong> for kids</h1>
+                    </div>
+
+                    <div class="col-lg-3 col-md-6 col-12"  data-aos-delay="200">
+                        <h4 class="my-4">Contact Info</h4>
+
+                        <p>
+                        <a href="#">
+                            <i class="fa fa-envelope mr-2 footer-icon"></i>
+                            dearanimal@gmail.com
+                        </a>
+                        </p>
+
+                    </div>
+
+                    <div class="col-lg-4 col-md-6 col-12"  data-aos-delay="300">
+                        <h4 class="my-4">Copyright &copy; Black Panther</h4>
+                    </div>
+
+                    
+                    </div>
+                </div>
+                </footer>
+            </dev>
+
+            ) : (
+
+                <dev className='anidetailbackground'>
                 <div className='detail-container'>
                 <img src={require('./images/animal_list/' + data.aniname + '_cover.jpeg')} class="img-fluid" />
                     <h1 className="bottom-right text-white unifont">{data.aniname}</h1>
@@ -106,6 +224,9 @@ class Anidetail extends Component{
                 </footer>
             </dev>
 
+
+            )}
+        </div>
       )}
     }
         
