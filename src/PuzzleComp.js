21e3089cf46b0346365ff09df6f280ef75bcb3aa
Ning <nche0024@gmail.com>
2020-05-17 23:39:56 +1000

    new

--- src/PuzzleComp.js
+++ src/PuzzleComp.js
@@ -21,7 +21,8 @@ class PuzzleComp extends Component{
     openPopupbox() {
         const content = (
           <div>
-            <p className='popupfont'>Congratulations! You win!</p>
+            <p className='popupfont'>Congratulations! </p>
+            <p className='popupfont story-des'>You win!</p>
           </div>
         )
         PopupboxManager.open({ content })
